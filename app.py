f24a0efdfc5d810363ade4514d9d3304249aefc8
Samuel Vasconcelos <smvasconcelos11@gmail.com>
2021-10-29 13:21:34 -0300

    Complementando interface e usando jquery

--- app.py
+++ app.py
@@ -2,24 +2,9 @@ from logging import debug
 from flask import Flask, render_template
 from datetime import datetime
 from flask_material import Material
+import webbrowser
 
 import re
-
-
-class Web:
-
-    search = False
-    progress = False
-    select = True
-
-    def set_search(self, value):
-        self.search = value
-
-    def set_progress(self, value):
-        self.search = value
-
-    def set_select(self, value):
-        self.select = value
 
 
 app = Flask(__name__)
@@ -29,7 +14,44 @@ Material(app)
 @app.route("/")
 def home():
     names = ['Ilhéus', 'Itabuna', 'Porto Seguro']
-    return render_template("pages/home.html", data=names, search=False)
+    estab_names = {
+
+        'Alana Hipermercado',
+        'Atacadao',
+        'Cestao da Economia',
+        'Itao Supermercado',
+        'Jaciana Supermercado',
+        'Gbarbosa',
+        'Frutaria e Mercadinho Cladudinete',
+        'Nenem Supermercados',
+        'Supermercado Meira - Malhado',
+        'Supermercado Meira - Centro',
+        'Supermercado Meira - N. Senhora da Vitoria',
+        'Supermercado Meira - Vilela',
+        'Supermercado Meira - N. Costa',
+        'Supermercado Mangostao',
+
+    }
+
+    return render_template("pages/home.html", data=names, search=False, product="Açúcar", estab_names=estab_names)
+
+
+@app.route("/batatinha")
+def run_script():
+    return "batatinha"
+
+
+# Insere a função para ser chamada em todos os templates a qualquer momento
+@app.context_processor
+def utility_processor():
+    def format_price(amount, currency="€"):
+        return f"{amount:.2f}{currency}"
+    return dict(format_price=format_price)
+
+
+@app.context_processor
+def inject_enumerate():
+    return dict(enumerate=enumerate)
 
 
 if __name__ == '__main__':
--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,105 @@
+var i = 0;
+function move() {
+	if (i == 0) {
+		i = 1;
+		var elem = document.getElementById("myBar");
+		var width = 10;
+		var id = setInterval(frame, 10);
+		function frame() {
+			if (width >= 100) {
+				clearInterval(id);
+				i = 0;
+			} else {
+				width++;
+				elem.style.width = width + "%";
+				elem.innerHTML = width + "%";
+			}
+		}
+	}
+}
+
+
 $(document).ready(function () {
-	$("selecionar-estab").click(function () {
-		$("progresso").addClass("active");
+
+	var city = undefined;
+
+	$('.estab').click((e) => {
+
+		let id = $(e.currentTarget).attr('id');
+
+		if ($(`#${id}`).hasClass('select-item-active')) {
+
+			$(`#${id}`).removeClass('select-item-active');
+
+		} else {
+
+			$(`#${id}`).addClass('select-item-active');
+
+		}
+
 	});
+
+	$('.city').click((e) => {
+
+		let id = $(e.currentTarget).attr('id');
+		console.log(city);
+		if (city === undefined) {
+			city = id;
+			$(`#${id}`).addClass('select-item-active');
+		} else {
+
+			if (city === id) {
+				$(`#${id}`).removeClass('select-item-active');
+				city = undefined;
+			} else {
+				Materialize.toast('Você só pode selecionar um municipio por vez ...', 1000);
+			}
+
+		}
+
+	});
+
+
+	$("#start").click(() => {
+
+		if (!$('.estab').hasClass("select-item-active")) {
+			Materialize.toast('Selecione pelo menos um item para prosseguir.', 2000);
+		} else {
+			Materialize.toast('Pesquisa iniciada ...', 1000);
+			$('ul.tabs').tabs('select_tab', 'progress');
+		}
+
+	});
+
+	$("#select").click(() => {
+
+		if (!$('.city').hasClass("select-item-active")) {
+			Materialize.toast('Selecione pelo menos um item para prosseguir.', 2000);
+		} else {
+			Materialize.toast('Pesquisa iniciada ...', 1000);
+			$('ul.tabs').tabs('select_tab', 'listagem');
+			$(".tabs a").addClass('disable');
+		}
+
+	});
+
+	$("#select-all").click(() => {
+
+		if (!$('.estab').hasClass("select-item-active")) {
+			$(`.estab`).addClass('select-item-active');
+		} else {
+			$(`.estab`).removeClass('select-item-active');
+		}
+
+	});
+
+	$("#back").click(() => {
+		$('ul.tabs').tabs('select_tab', 'pesquisar');
+		$(".tabs a").addClass('enable');
+	});
+
+	// Exemplo de requisição para o python
+	// $.get("/batatinha", function (data) {
+	// 	console.log('batata');
+	// });
 });
